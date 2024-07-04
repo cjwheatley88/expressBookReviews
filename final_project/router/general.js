e87e6c2330114209c15c1879b53343ef73f05165
Chris W <cjwheatley88@gmail.com>
2024-07-04 18:52:22 -0400

    up to task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,14 +28,21 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+
+    for(let i = 0; i < books.length; i++) {
+    let obj = books[i].author;
+
+        if (obj === author) {
+            return res.send(books[author]);
+        }
+};
 });
 
 // Get all books based on title
