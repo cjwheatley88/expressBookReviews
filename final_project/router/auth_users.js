0e1503081a70203ae7a8d8a003a0bb8acc41e164
Chris Wheatley <cjwheatley88@gmail.com>
2024-07-05 19:59:10 -0400

    code added for registered user validation

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -2,21 +2,66 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
+const session = require('express-session');
 
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
-}
+    let validusers = users.filter((user) => {
+        return (user.unsername === username)
+    });
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+// Check if the user with the given username and password exists
+const authenticatedUser = (username, password) => {
+    // Filter the users array for any user with the same username and password
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    // Return true if any valid user is found, otherwise false
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
 
 //only registered users can login
+const app = express();
+app.use(session({secret:"fingerprint"},resave=true,saveUninitialized=true));
+app.use(express.json());
+
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  //err cx - missing username or password
+  if (!username || !password) {
+    return res.status(404).json({ message: "Error logging in, missing credentials."})
+  }
+
+  //auth user
+  if (authenticatedUser(username, password)) {
+    //JWT Generation
+    let accessToken = jwt.sign({
+        data: password
+    }, 'access', { expiresIn: 60 * 60 });
+
+    //record details within session
+    req.session.authorization = {
+        accessToken, username
+    }
+    return res.status(200).send("User log in - success!");
+  } else {
+    return res.status(208).json({message: "User login unsuccessful - Check username and password."});
+  }
 });
 
 // Add a book review
